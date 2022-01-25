46c781cb86d2cc66fabd8c24d4c5a75a615d4e26
vivek koyani <vivek.koyani@gmail.com>
2022-01-25 22:39:50 +0530

    add todo list

---
+++ todo_list_app_1/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import TodoLists from './TodoLists';
+
+function App() {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value)
+  }
+
+  const listofItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList]
+    })
+    setInputList('');
+  }
+
+  const deleteItems = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    })
+  }
+  return (
+    <div className="main_app">
+      <div className='center_div'>
+        <br />
+        <h1>ToDo List</h1>
+        <br />
+        <input type="text" placeholder='Add a Items' onChange={itemEvent} />
+        <button onClick={() => {listofItems()}}> + </button>
+
+        <ol>
+          {/* <li>{inputList}</li> */}
+          {Items.map((itemval, index) => {
+            return <TodoLists key={index} id={index} text={itemval} onSelect={deleteItems} />
+          })}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default App;
